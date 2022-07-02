8b5eb22a64c577a5067690a2f23fb6bab351249b
Aniket Kokate <projukees@gmail.com>
2022-07-02 02:13:10 +0100

    Made Getting Numbers Functionality

---
+++ GetNumbers.py
@@ -0,0 +1,37 @@
+import sys, getopt
+
+class CancelProgram(Exception):
+   pass
+
+def GetNumbers(argv):
+   print("THIS PROGRAM WILL ONLY WORK WITH VERIZON PHONE NUMBERS")
+   print("SUPPORT FOR OTHER NUMBER WILL BE COMING SOON\n\n\n")
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hp:m:",["phoneNumber=","message="])
+   except getopt.GetoptError:
+      print ('USAGE: ./TextPhone.sh -p <Phone Number> -m <Message surrounded by quotes>')
+      sys.exit(2)
+   if len(sys.argv) == 1:
+         print ('USAGE: ./TextPhone.sh -p <Phone Number> -m <Message surrounded by quotes>')
+         sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('USAGE: ./TextPhone.sh -p <Phone Number> -m <Message surrounded by quotes>')
+         sys.exit()
+      elif opt in ("-p", "--phoneNumber"):
+         phoneNumber = arg
+      elif opt in ("-m", "--message"):
+         message = arg
+   
+   try:
+      print ('Phone Number is', phoneNumber)
+      print (f'Message is "{message}"')
+      if input("Are you sure that you want to send this? y/n  ") == "n": raise CancelProgram
+   except CancelProgram: pass
+   except: print("\nERROR: ALL VALUES NOT PROVIDED \nUSAGE: print ('./TextPhone.sh -p <Phone Number> -m <Message surrounded by quotes>')")
+
+
+if __name__ == "__main__":
+   GetNumbers(sys.argv[1:])
